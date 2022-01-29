2918c6213b59956e666231cd306d7f7aa28abeae
sfif7890 <dvornikov.sashakpc1882020@gmail.com>
2022-01-29 21:26:03 +0400

    x3_1

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 list=[]
 
 def input_num():
-    pass
+    pass #я
 
 def x2(x):
-    pass
-
-def x3_1():
-    pass
-
+    pass #я
+def x3_1(x):
+    x = x*3+1
+    list.append(int(x))
+    return collatz(x)
 def collatz():
-    pass
+    pass #Саня
 
-input_num()
+input_num() 
