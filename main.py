4cdf9d250c08f54d011f3931e42d23100e2ccfd3
Guardian112 <98177496+Guardian112@users.noreply.github.com>
2022-01-29 19:01:56 +0300

    Заготовка 1

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-if __name__ == '__main__':
-    print('Hello World')
+list=[]
+
+def input_num():
+    pass #я
+
+def x2(x):
+    pass #я
+def x3_1():
+    pass #Саня
+def collatz():
+    pass #Саня
+
+input_num() 
